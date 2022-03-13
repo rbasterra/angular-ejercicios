1d03c2a048e7f2c4cff6511746b1d730a1babb1c
Rafael Basterra Careaga <rbasterra@msn.com>
2022-03-13 11:24:32 +0100

    added TLS to marvel api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   marvelApiKey: 'f2ed942d9fc733f4bbed3601167ddcb7',
-  marvelApiUrl: 'http://gateway.marvel.com/v1/public/',
+  marvelApiUrl: 'https://gateway.marvel.com/v1/public/',
   imgSize: 'portrait_fantastic',
   userApiUrl: 'https://angular-project-server.herokuapp.com'
 };
