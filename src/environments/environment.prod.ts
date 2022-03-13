cbaf0a2e48b3774e6dd16025376d123397cd602e
Rafael Basterra Careaga <rbasterra@msn.com>
2022-03-13 13:45:16 +0100

    testing environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,4 @@
 export const environment = {
   production: true,
-  marvelApiKey: 'f2ed942d9fc733f4bbed3601167ddcb7',
-  marvelApiUrl: 'https://gateway.marvel.com/v1/public/',
-  imgSize: 'portrait_fantastic',
-  userApiUrl: 'https://angular-project-server.herokuapp.com'
+  
 };
